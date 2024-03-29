3865a9558b96bc2cdc08aa65fae0d372489000e3
shobhitexe <sguptamds94@gmail.com>
2024-03-29 20:37:21 +0530

    Smooth scroll duration increase

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -19,7 +19,7 @@ import { useEffect } from "react";
 
 export default function Home() {
   useEffect(() => {
-    const lenis = new Lenis();
+    const lenis = new Lenis({ duration: 5 });
 
     function raf(time: number) {
       lenis.raf(time);
